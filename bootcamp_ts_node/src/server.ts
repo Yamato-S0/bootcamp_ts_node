2c06ce1dfb7278c71ffe4d217376bdaab8ad3585
Yamato <shinohara559@gmail.com>
2023-07-13 00:23:09 +0900

    updated

--- bootcamp_ts_node/src/server.ts
+++ bootcamp_ts_node/src/server.ts
@@ -7,9 +7,34 @@ server.on("request", async (req, res) => {
   console.log("request url: ", req.url);
   // Content-Type is important for browsers.
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
-  res.writeHead(200, { "content-type": "text/plain" });
-  res.write("hello!\n");
-  res.end();
+  // リクエストされたパスを取得
+  const filePath = path.join(__dirname, "public", String(req.url));
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-type": "text/plain" });
+      res.end();
+    } else {
+      // ファイルの拡張子に基づいて適切なContent-Typeを設定する
+      let contentType = "text/plain";
+      if (filePath.endsWith(".html")) {
+        contentType = "text/html";
+      } else if (filePath.endsWith(".jpg")) {
+        contentType = "image/jpeg";
+      } else if (filePath.endsWith(".json")) {
+        contentType = "text/json";
+      } else if (filePath.endsWith(".ico")) {
+        contentType = "image/x-icon";
+      }
+
+      res.writeHead(200, { "Content-type": contentType});
+      res.write(data);
+      res.end();
+    }
+  });
+  //res.writeHead(200, { "content-type": "text/plain" });
+  //res.write("hello!\n");
+  //res.end();
 });
 
 server.on("listening", () => {
@@ -17,7 +42,11 @@ server.on("listening", () => {
 });
 
 // Start listening 12345 port of localhost (127.0.0.1).
-server.listen(12345, () => {
-  console.log("listening on http://localhost:12345/");
+//server.listen(12345, () => {
+  //console.log("listening on http://localhost:12345/");
+//});
+const port = process.env.PORT || 12345;
+server.listen(port, () => {
+  console.log(`listening on http://localhost:${port}/`);
 });
 console.log("run server.js");
